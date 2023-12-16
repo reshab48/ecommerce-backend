887168367943ce94739910ce4d6ecc0e68a5e74e
Reshab Das <das.reshab48@gmail.com>
2023-12-16 13:13:50 +0530

    Add product admin

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
